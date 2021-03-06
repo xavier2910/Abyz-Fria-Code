54c2bc1b34f1b75df6d136e6901fe47caa8a2b51
xavier2910 <x.distelzweig@gmail.com>
2021-03-06 14:18:22 -0600

    created SequentialCommandExecuter

--- abyz_fria/Commands/CommandExecuter.cpp
+++ abyz_fria/Commands/CommandExecuter.cpp
@@ -4,41 +4,21 @@
 #include <StandardCplusplus.h>
 #include <queue>
 #include <utility.h>
-#include <functional>
 
 #include "Command.cpp"
 
 class CommandExecuter : public Command
 {
-private:
-    std::queue<std::pair<Command *, std::unary_function<bool, bool>>> _commands; //TODO: create own type for function
+protected:
+    std::queue<std::pair<Command *, bool (*)()>> _commands;
 
 public:
-    virtual void addCommand(Command *, std::unary_function<bool, bool>);
+    virtual void addCommand(Command *, bool (*)());
 };
 
-void CommandExecuter::addCommand(Command *command, std::unary_function<bool, bool> nullaryPredicate)
+void CommandExecuter::addCommand(Command *command, bool (*nullaryPredicate)())
 {
     _commands.push(std::make_pair(command, nullaryPredicate));
 }
 
-/* TODO: move to SequentialCommandExecuter
-void SequentialCommandExecuter::execute()
-{
-    while (!_commands.empty())
-    {
-        bool dummy = false;
-
-        if ((_commands.front().second)(dummy))
-        {
-            _commands.front().first->begin();
-        }
-        if (_commands.front().first->canExecute())
-        {
-            _commands.front().first->execute();
-            _commands.pop();
-        }
-    }
-}
-*/
 #endif
---
+++ abyz_fria/Commands/SequentialCommandExecuter.cpp
@@ -0,0 +1,22 @@
+#ifndef SequentialCommandExecuter_h
+#define SequentialCommandExecuter_h
+
+#include "CommandExecuter.cpp"
+
+class SequentialCommandExecuter : public CommandExecuter
+{
+    void execute() override
+    {
+        if ((_commands.front().second)())
+        {
+            _commands.front().first->begin();
+        }
+        _commands.front().first->operator()();
+        if (_commands.front().first->isFinished())
+        {
+            _commands.pop();
+        }
+    }
+};
+
+#endif
