cbcdab80da4d398a30d5898809968c153a21a96a
xavier2910 <x.distelzweig@gmail.com>
2021-03-06 14:17:29 -0600

    continued testing removal

--- abyz_fria/abyz_fria.ino
+++ abyz_fria/abyz_fria.ino
@@ -1,14 +1,12 @@
 #include "Abyz_Fria.cpp"
 
-#define DHT_PIN 2 //TODO: set this pin
-
 Abyz_Fria rocket;
 
 void setup()
 {
     Serial.begin(9600);
 
-        rocket.main();
+    rocket.main();
 }
 
 void loop()
